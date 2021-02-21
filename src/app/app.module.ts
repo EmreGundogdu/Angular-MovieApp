708557fca8d54c82f99197ca96f9c426c18456bb
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-02-21 14:32:43 +0300

    Created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MovieComponent } from './movie/movie.component';
+import { MoviesComponent } from './movies/movies.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+      MovieComponent,
+      MoviesComponent
+   ],
   imports: [
     BrowserModule
   ],
