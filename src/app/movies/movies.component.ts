18a4866d107cc826fd43097dd558a4b94b88ef34
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-02-22 21:04:08 +0300

    Directives

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,27 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { Movie } from '../movie';
+import { Movies } from '../movie.datasource';
 
 @Component({
   selector: 'movies',
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.scss'],
-  template: `<h2>{{Ttile : getTtitle()}}</h2>
-    <div>
-      {{ movie.id }}/div>
-      <div>{{ movie.name }}/div></div>
-    </div> `,
 })
 export class MoviesComponent implements OnInit {
   title = 'Movie List';
+  movies = Movies;
 
-  movie: Movie = {
-    id: 1,
-    name: 'Movie Name',
-  };
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() { }
+  ngOnInit() {}
   getTitle() {
     return this.title;
   }
