7c38d2eb968827c2208af217d037d9a37b5e8d31
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-02-26 22:07:59 +0300

    dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from '../movie';
+import { MovieService } from '../movie.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  movies: Movie[]=[];
+  movieLength!:number;
+
+  constructor(private movieService: MovieService) { }
 
   ngOnInit(): void {
+    this.getMovies();
+  }
+  getMovies():void{
+    this.movieService.getMovies()
+    .subscribe(movies=>{
+      this.movies = movies.slice(0,5)
+      this.movieLength = movies.length;
+    })
   }
 
 }
--- src/app/movie.datasource.ts
+++ src/app/movie.datasource.ts
@@ -6,6 +6,7 @@ export const Movies: Movie[]=[
   {id:3,name:"movie 3",description:"Kötü Film",imageUrl:"3.jpg"},
   {id:4,name:"movie 4",description:"Bu Film Olmamış",imageUrl:"4.jpg"},
   {id:5,name:"movie 5",description:"Başarılı ve Ödüllü Bir Film",imageUrl:"5.jpg"},
-  {id:6,name:"movie 6",description:"Ödüllü Bir Film",imageUrl:"5.jpg"},
-  {id:7,name:"movie 7",description:"Başarılı Bir Film",imageUrl:"5.jpg"},
+  {id:6,name:"movie 6",description:"Başarılı Bir Film",imageUrl:"6.jpg"},
+  {id:7,name:"movie 7",description:"Ödüllü Bir Film",imageUrl:"7.jpg"}
+
 ];
